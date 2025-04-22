5ff591b8701edd4c3a05074ba5238641c6dcaa95
mmsalim <happymarja@gmail.com>
2025-04-22 19:33:25 +0530

    feat: expense tracker

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { HistoryComponent } from './components/history/history.component';
 import { ExpensesComponent } from './components/expenses/expenses.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
